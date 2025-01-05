458a8de0632bdf4a9c7e9cffdea44067bb767105
mrDezz <deepakguptaswork@gmail.com>
2025-01-05 17:06:05 +0530

    firebase setup done

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,11 +1,49 @@
 import { View, Text } from "react-native";
+import auth from "@react-native-firebase/auth";
+
 import React from "react";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+GoogleSignin.configure({
+  webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+});
 
 const Login = () => {
+  async function handleGoogleSignIn() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const signInResult: any = await GoogleSignin.signIn();
+
+    // Try the new style of google-sign in result, from v13+ of that module
+    let idToken = signInResult.data?.idToken;
+    if (!idToken) {
+      // if you are using older versions of google-signin, try old style result
+      idToken = signInResult.idToken;
+    }
+    if (!idToken) {
+      throw new Error("No ID token found");
+    }
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(
+      signInResult.data.token
+    );
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <GoogleSigninButton
+      size={GoogleSigninButton.Size.Wide}
+      color={GoogleSigninButton.Color.Dark}
+      onPress={() => {
+        handleGoogleSignIn();
+      }}
+      // disabled={isInProgress}
+    />
   );
 };
 
